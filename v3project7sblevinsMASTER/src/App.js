411169e53005cf23afef0d8f12e0ea553340a48a
Sara Blevins <dvlnthebludrs@gmail.com>
2018-11-03 12:53:46 -0400

    Change to simple div sidebar

--- v3project7sblevinsMASTER/src/App.js
+++ v3project7sblevinsMASTER/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Map from './Map';
 
@@ -7,18 +6,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-        <p>This is a header</p>
-        </header>
-        <nav id="drawer" class="nav">
-          <ul class="nav__list">
-            <li class="nav__item"><a href="#">News</a></li>
-            <li class="nav__item"><a href="#">Events</a></li>
-            <li class="nav__item"><a href="#">Culture</a></li>
-            <li class="nav__item"><a href="#">Blog</a></li>
-          </ul>
-        </nav>
-        <Map />
+      <div className="nav">Explore!
+      <ul>venue one</ul>
+            <ul>venue two</ul>
+            <ul>venue three</ul>
+            <ul>venue four</ul>
+            <ul>venue five</ul>
+      </div>
+      <Map />
       </div>
     );
   }
