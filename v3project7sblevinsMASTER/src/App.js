66a7d7b8b76764f9782e91e2faca55eddf7c7a78
Sara Blevins <dvlnthebludrs@gmail.com>
2018-11-03 12:58:24 -0400

    Move sidebar to a component

--- v3project7sblevinsMASTER/src/App.js
+++ v3project7sblevinsMASTER/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './Map';
+import Sidebar from './Sidebar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <div className="nav">Explore!
-      <ul>venue one</ul>
-            <ul>venue two</ul>
-            <ul>venue three</ul>
-            <ul>venue four</ul>
-            <ul>venue five</ul>
-      </div>
+      <Sidebar />
       <Map />
       </div>
     );
---
+++ v3project7sblevinsMASTER/src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Sidebar extends React.Component {
+    render() {
+        return (
+            <div className="nav">Explore!
+      <ul>venue one</ul>
+                <ul>venue two</ul>
+                <ul>venue three</ul>
+                <ul>venue four</ul>
+                <ul>venue five</ul>
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
